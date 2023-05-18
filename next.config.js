d3e4e77b1010abde7eb0d6e374e4d4f9984c5f1c
Heiner000 <garrett.heiner@gmail.com>
2023-05-18 12:00:47 -0700

    init commit :

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // do not use https://
+    // array of WHITELISTED image domains
+    domains: ["placekitten.com"]
+  }
 }
 
 module.exports = nextConfig
