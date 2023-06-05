82122d3be64a4945927efdcafc0fb08f2dd93104
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-06-05 00:39:55 -0700

    updates jsconfig.json for vercel build

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Profile from '@/components/profile'
 import ProjectPreview from '@/components/project_preview'
 import Skills from '@/components/skills'
